6418413f6195aed8fd23871e8ce36ba707a5b771
AdelinChelariu <adelinchelariu@gmail.com>
2023-11-06 00:29:15 +0200

    Am mai facut ceva pe la clasa Produs si la Casca

--- MagazinEchipamenteMoto/src/Casca.java
+++ MagazinEchipamenteMoto/src/Casca.java
@@ -1,5 +1,56 @@
-public class Casca {
-    private char[] dimensiune;  // XS -> XXL
-    //comment
-    private String tip;
+public class Casca extends Produs {
+    private char[] dimensiune;  // XXS -> XXL
+    private String tip;         // Integrala, Flip-Up, etc...
+    private String culoare;     // Culoarea / Design-ul castii
+
+    public char[] getDimensiune() {
+        return dimensiune;
+    }
+
+    public void setDimensiune(char[] dimensiune) {
+        this.dimensiune = dimensiune;
+    }
+
+    public String getTip() {
+        return tip;
+    }
+
+    public void setTip(String tip) {
+        this.tip = tip;
+    }
+
+    public String getCuloare() {
+        return culoare;
+    }
+
+    public void setCuloare(String culoare) {
+        this.culoare = culoare;
+    }
+    
+    public Casca(String denumire, String descriereFull, String descriereShort, String marca, String imagine, float pret, boolean disponibilitate, char[] dimensiune, String tip, String culoare) {
+        super(denumire, descriereFull, descriereShort, marca, imagine, pret, disponibilitate);
+        this.dimensiune = dimensiune;
+        this.tip = tip;
+        this.culoare = culoare;
+    }
+
+    public Casca(Casca c){
+        this.setDenumire(c.getDenumire());
+        this.setDescriereFull(c.getDescriereFull());
+        this.setDescriereShort(c.getDescriereShort());
+        this.setMarca(c.getMarca());
+        this.setImagine(c.getImagine());
+        this.setPret(c.getPret());
+        this.setDisponibilitate(c.getDisponibilitate());
+        this.dimensiune = c.dimensiune;
+        this.tip = c.tip;
+        this.culoare = c.culoare;
+    }
+
+    public Casca() {
+        super();
+        this.dimensiune = null;
+        this.tip = null;
+        this.culoare = null;
+    }
 }
--- MagazinEchipamenteMoto/src/Produs.java
+++ MagazinEchipamenteMoto/src/Produs.java
@@ -2,70 +2,95 @@
 public class Produs {
 
     private String denumire;            // Numele produsului
-    private String descriere;           // O descriere, ceva despre produs
+    private String descriereFull;       // O descriere completa a produsului
+    private String descriereShort;      // O descriere scurta a produsului, pentru pagina principala
     private String marca;               // Marca produsului
     private String imagine;             // Calea catre o imagine cu produsul
     private float pret;                 // Pretul produsului
     private boolean disponibilitate;    // Se afla sau nu in stoc 
-    
+
     // Setteri si  getteri pentru datele membre
-    public String getDenumire(){
+    public String getDenumire() {
         return denumire;
     }
-    public void setDenumire(String denumire){
+
+    public void setDenumire(String denumire) {
         this.denumire = denumire;
     }
-    public String getDesrirere(){
-        return descriere;
+
+    public String getDescriereFull() {
+        return descriereFull;
     }
-    public void setDescriere(String descriere){
-        this.descriere = descriere;
+
+    public void setDescriereFull(String descriereFull) {
+        this.descriereFull = descriereFull;
     }
-    public String getMarca(){
+
+    public String getDescriereShort() {
+        return descriereShort;
+    }
+
+    public void setDescriereShort(String descriereShort) {
+        this.descriereShort = descriereShort;
+    }
+
+    public String getMarca() {
         return marca;
     }
-    public void setMarca(String marca){
+
+    public void setMarca(String marca) {
         this.marca = marca;
     }
-    public String getImagine(){
+
+    public String getImagine() {
         return imagine;
     }
-    public void setImagine(String imagine){
+
+    public void setImagine(String imagine) {
         this.imagine = imagine;
     }
-    public float getPret(){
+
+    public float getPret() {
         return pret;
     }
-    public void setPret(float pret){
+
+    public void setPret(float pret) {
         this.pret = pret;
     }
-    public boolean getDisponibilitate(){
+
+    public boolean getDisponibilitate() {
         return disponibilitate;
     }
-    public void setDisponibilitate(boolean disponibilitate){
+
+    public void setDisponibilitate(boolean disponibilitate) {
         this.disponibilitate = disponibilitate;
     }
-    
+
     // Constructori pentru obiecte de tip produs
-    public Produs(){
+    public Produs() {
         this.denumire = null;
-        this.descriere = null;
+        this.descriereFull = null;
+        this.descriereShort = null;
         this.marca = null;
         this.imagine = null;
         this.pret = 0;
         this.disponibilitate = false;
     }
-    public Produs(String denumire, String descriere, String marca, String imagine, float pret, boolean disponibilitate) {
+
+    public Produs(String denumire, String descriereFull, String descriereShort, String marca, String imagine, float pret, boolean disponibilitate) {
         this.denumire = denumire;
-        this.descriere = descriere;
+        this.descriereFull = descriereFull;
+        this.descriereShort = descriereShort;
         this.marca = marca;
         this.imagine = imagine;
         this.pret = pret;
         this.disponibilitate = disponibilitate;
     }
-    public Produs(Produs p){
+
+    public Produs(Produs p) {
         this.denumire = p.denumire;
-        this.descriere = p.descriere;
+        this.descriereFull = p.descriereFull;
+        this.descriereShort = p.descriereShort;
         this.marca = p.marca;
         this.imagine = p.imagine;
         this.pret = p.pret;
