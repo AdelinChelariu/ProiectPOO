58ef129738cc43e06f8ded25d5e8cde739d6f7e9
AdelinChelariu <adelinchelariu@gmail.com>
2023-11-13 20:33:17 +0200

    Am adaugat clasa Main, pentru testari si afisari si restu

---
+++ MagazinEchipamenteMoto/src/Main.java
@@ -0,0 +1,8 @@
+//Aici vom face declaratii, afisari, si tot ce mai testam :3
+//  uwu
+public class Main {
+    public static void main(String[] args){
+        Casca c1 = new Casca();
+        System.out.println(c1.getDenumire());
+    }
+}
