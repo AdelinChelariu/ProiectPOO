8b3319cc12a5ff487b2173815ebf350bbaa735ad
ION2Q4354658870 <caramarinionut515@gmail.com>
2023-11-20 17:09:17 +0200

    AAAAAA

--- MagazinEchipamenteMoto/src/SuportTelefonMobil.java
+++ MagazinEchipamenteMoto/src/SuportTelefonMobil.java
@@ -1,12 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author beligga
- */
 public class SuportTelefonMobil extends Produs {
     private String prindere; //tip prindere: oglinda, ghidon
     private String material; //plastic, metal
