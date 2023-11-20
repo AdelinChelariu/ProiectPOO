e6fd2d6d5038d4271b816bd671e6c828c6a885dc
yoolean69 <iuleak971@gmail.com>
2023-11-20 16:04:13 +0200

    yes

--- MagazinEchipamenteMoto/src/MagazinGUI.java
+++ MagazinEchipamenteMoto/src/MagazinGUI.java
@@ -13,11 +13,11 @@ public class MagazinGUI extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 496, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 347, Short.MAX_VALUE)
         );
 
         pack();
